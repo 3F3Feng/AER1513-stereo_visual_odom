9c77cea622b2023bcf8a571138353fd0079450dc
3F3Feng <43562203+3F3Feng@users.noreply.github.com>
2020-07-16 14:33:37 -0400

    add helper function

---
+++ compute_motion_SVD.m
@@ -0,0 +1,50 @@
+% Date created: Nov-19
+% Author: Miller
+
+% This is the core function that computes motion from two pointclouds
+function [C, r] = compute_motion_SVD( p1, p2 )
+
+    % ------insert your motion-from-two-pointclouds algorithm here------
+    
+    % ASSUME: All weights are one
+    
+    % calculate the centoid of the point cloud 1
+    count = 0;
+    center_1 = zeros(3,1);
+    for i = 1 : size(p1,2)
+        center_1 = center_1 + p1(:,i);
+        count = count + 1;
+    end
+    center_1 = center_1 ./ count;
+    
+    % calculate the centoid of the point cloud 2
+    count = 0;
+    center_2 = zeros(3,1);
+    for i = 1 : size(p2,2)
+        center_2 = center_2 + p2(:,i);
+        count = count + 1;
+    end
+    center_2 = center_2 ./ count;
+    
+    % Calculate the W matrix 3 by 3 matrix
+    % loop through every pair
+    W = zeros(3,3);
+    count = 0;
+    for i = 1 : size(p1,2)
+        W = W + ( p2(:,i) - center_2 ) * ( p1(:,i) - center_1)';
+        count = count + 1;
+    end
+    W = W ./ count;
+    
+    [U, ~, V] = svd(W);
+    
+    C = U * [1 0 0; 0 1 0 ; 0 0 det(U)*det(V)] * V';     % temporary to make script run
+    r = - C' * center_2 + center_1; % temporary to make script run
+   
+    
+    
+    % ------end of your motion-from-two-pointclouds algorithm-------
+    
+end
+
+
---
+++ matchfeatures_SURF.m
@@ -0,0 +1,90 @@
+function [matchedPts1, matchedPts2, fea_ord_1, fea_ord_2,err] = matchfeatures_SURF(features_1,features_2,thsErr)
+% matchfeatures_SURF returns matched features between two images using the
+% sum-of-squared-difference (SSD) metric
+% Reference: matchSURFfeaturesTB
+%
+%   mathcfeatures_SURF(features1,features2) computes the SSD between
+%   OpenSurf features and then uses a threshold error to create two
+%   matrices containing the coordinates of corresponding features
+%
+%   Inputs:
+%   -------
+%   features1   - OpenSurf features for image 1 (class: 'struct')
+%   features2   - OpenSurf features for image 2 (class: 'struct'
+%
+%   Outputs:
+%   -------
+%   matchedPts1    - x,y coordinates of matched points in image 1
+%                       corresponding to points in image 2
+%   matchedPts2    - x,y coordinates of matched points in image 2
+%                       corresponding to points in image 1
+%
+%
+%
+% Author: Miller Tang
+% Date Created: 2019-05-11
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Step 1: Extract the feature descriptors from the input data structure and
+% put the descriptors into two matrices
+    Feat_des1 = reshape([features_1.descriptor], 64, []);
+    Feat_des2 = reshape([features_2.descriptor], 64, []);
+
+
+% Step 2: Find best matches using sum-of-squared-difference (SSD)
+% Define the maximum accceptable error to return points as a match
+  %thsErr = 0.05; %%%% threshold of error to accept as a match [change]
+
+% Find the best matches using the sum-of-squared-difference metric
+  err=zeros(1,length(features_1));
+  cor1=1:length(features_1); 
+  cor2=zeros(1,length(features_1));
+  % Here is the key step to find matches:
+  % For each feature (with 64 directions) detected in image 1, we define an
+  % array that repeat that specific feature many times to the same size as
+  % the number of features detected in image 2. Every feature in image 2
+  % also has 64 directions, and thus we can compute the SSD between the one
+  % feature found in image 1 to every feature descriptor in image 2 to
+  % obtain an array of error (i.e. SSD). we pick the pair with the lowest
+  % SSD and restore the index of feature in image 2 to cor2(i), and also
+  % the error term into err(i). We loop through all the feature descriptors
+  % found in image 1 to complete the matching process.
+  for i=1:length(features_1)
+      distance=sum((Feat_des2-repmat(Feat_des1(:,i),[1 length(features_2)])).^2,1);  
+      [err(i),cor2(i)]=min(distance); % the 'distance' variable is a row vector of sum of each column (i.e. sum of the squared difference)
+  end
+  
+% Step 3: Reorder the error terms in ascending order
+  [err, ind]=sort(err); 
+  cor1=cor1(ind); 
+  cor2=cor2(ind);
+  
+% Step 4: Using the threshold defined to determine how many matches are
+% below the threshold
+  check_thresh = err < thsErr; % return 0 or 1 as boolean
+  num_matches = sum(check_thresh); % return a "double" type integer showing how many matches below threshold
+ 
+% Step 5: Restore the matches that are below threshold into new variables
+  matchedPts1 = zeros(num_matches,2);
+  matchedPts2 = zeros(size(matchedPts1));
+  
+% Step 6: Return the top matches following the SSD order
+  for i=1:num_matches
+      %c=rand(1,3);
+      %plot([features_1(cor1(i)).x features_2(cor2(i)).x+size(I1,2)],[features_1(cor1(i)).y features_2(cor2(i)).y],'-','Color',c)
+      %plot([features_1(cor1(i)).x features_2(cor2(i)).x+size(I1,2)],[features_1(cor1(i)).y features_2(cor2(i)).y],'o','Color',c)
+      matchedPts1(i,1) = features_1(cor1(i)).x; % First column is x coor.
+      matchedPts1(i,2) = features_1(cor1(i)).y; % Second column is y coor.
+      fea_ord_1(i) = features_1(cor1(i));
+      
+      matchedPts2(i,1) = features_2(cor2(i)).x; % First column is x coor.
+      matchedPts2(i,2) = features_2(cor2(i)).y; % Second column is y coor.
+      fea_ord_2(i) = features_2(cor2(i));
+  end
+  
+  %fea_ord_1 = features_1(cor1);
+  %fea_ord_2 = features_2(cor2);
+  
+end
